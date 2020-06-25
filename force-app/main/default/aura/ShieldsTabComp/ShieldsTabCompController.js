2e7e07ab2cfc51d39488ce3461bcc5a0ff2ff843
AsewAsew <dylanburk97@gmail.com>
2020-06-24 19:03:12 -0500

    Finished Shield Tab component

--- force-app/main/default/aura/ShieldsTabComp/ShieldsTabCompController.js
+++ force-app/main/default/aura/ShieldsTabComp/ShieldsTabCompController.js
@@ -1,5 +1,30 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
+        helper.archetypes(component);
+        helper.shields(component);
+    },
 
+    nextPage : function(component, event, helper) {
+        component.set("v.page", component.get("v.page") + 1);
+        helper.shields(component);
+    },
+
+    selectType : function(component, event, helper) {
+        component.set("v.archetype", event.currentTarget.value);
+
+        helper.resetPage(component);
+        helper.shields(component);
+    },
+
+    selectMaterial : function(component, event, helper) {
+        component.set("v.material", event.currentTarget.value);
+
+        helper.resetPage(component);
+        helper.shields(component);
+    },
+
+    prevPage : function(component, event, helper) {
+        component.set("v.page", component.get("v.page") - 1);
+        helper.shields(component);
     }
 })
--- force-app/main/default/aura/ShieldsTabComp/ShieldsTabCompHelper.js
+++ force-app/main/default/aura/ShieldsTabComp/ShieldsTabCompHelper.js
@@ -1,5 +1,75 @@
 ({
-    helperMethod : function() {
+    archetypes : function(component) {
+        var action = component.get("c.getShiArchs");
 
+        action.setCallback(this, function(response) {
+            if(response.getState() === "SUCCESS")
+            {
+                var result = response.getReturnValue();
+                component.set("v.shiArchs", result);
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    shields : function(component) {
+        var action = component.get("c.getShields");
+
+        action.setParams({
+            page: component.get("v.page"),
+            perPage: component.get("v.perPage"),
+            archetype: component.get("v.archetype"),
+            material: component.get("v.material")
+        });
+
+        action.setCallback(this, function(response) {
+            if(response.getState() === "SUCCESS")
+            {
+                var result = response.getReturnValue();
+                component.set("v.shields", result);
+                this.shieldCount(component);
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    shieldCount : function(component) {
+        var action = component.get("c.getShieldCount");
+
+        action.setParams({
+            archetype: component.get("v.archetype"),
+            material: component.get("v.material")
+        });
+
+        action.setCallback(this, function(response) {
+            if(response.getState() === "SUCCESS")
+            {
+                var result = response.getReturnValue();
+                component.set("v.shieldCount", result);
+                this.disable(component);
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    disable : function(component){
+        var page = component.get("v.page");
+        var perPage = component.get("v.perPage");
+        var shieldCount = component.get("v.shieldCount");
+        if(page < 1)
+        {
+            page = 1;
+            this.resetPage(component);
+        }
+
+        component.set("v.disPrev", page == 1);
+        component.set("v.disNext", shieldCount <= page * perPage);
+    },
+
+    resetPage : function(component) {
+        component.set("v.page", 1);
     }
 })
